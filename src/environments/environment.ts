e7618b189c542a6d4c42c24faa78da997e18077d
steffenjoachim <steffenjoachim.schanze@gmail.com>
2023-07-20 08:51:08 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'crudapp-todolist',
+    appId: '1:638294973314:web:b1a08674787a6d0a5ecd9b',
+    storageBucket: 'crudapp-todolist.appspot.com',
+    apiKey: 'AIzaSyB2CVGfkYu0x5IAtRqtIk77rn5U-7kkh4c',
+    authDomain: 'crudapp-todolist.firebaseapp.com',
+    messagingSenderId: '638294973314',
+  },
+};
